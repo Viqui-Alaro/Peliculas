e683c342d12f4cb4df3165f6992ff62c62bcab98
Viqui-Alaro <viquialaro241@gmail.com>
2020-05-23 21:47:59 -0400

    validacion cuando no encuentra ninguna imagen

--- src/app/pipes/imagen.pipe.ts
+++ src/app/pipes/imagen.pipe.ts
@@ -10,9 +10,9 @@ export class ImagenPipe implements PipeTransform {
   transform(img: string, size: string = 'w500'): string {
 
     if (!img){
-      return;
+      return './assets/no-image-banner.jpg';
     }
-    const imgUrl = `${ URL }/${ size }/${ img }`;
+    const imgUrl = `${ URL }/${ size }${ img }`;
     console.log('URL', imgUrl);
 
     return imgUrl;
