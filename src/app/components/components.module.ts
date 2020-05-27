b78216df1d4b0e37906580af2cc27daa077b26bd
Viqui-Alaro <viquialaro241@gmail.com>
2020-05-26 21:37:01 -0400

    implementacion de componente para el slide show

---
+++ src/app/components/components.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { SlideshowBackdropComponent } from './slideshow-backdrop/slideshow-backdrop.component';
+import { PipesModule } from '../pipes/pipes.module';
+
+
+
+@NgModule({
+  declarations: [
+    SlideshowBackdropComponent
+  ],
+  exports: [
+    SlideshowBackdropComponent
+  ],
+  imports: [
+    CommonModule,
+    IonicModule,
+    PipesModule
+  ]
+})
+export class ComponentsModule { }
