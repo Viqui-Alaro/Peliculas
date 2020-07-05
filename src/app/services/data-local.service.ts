4442a65994fc7b9df6d326162073a200965ed168
Viqui-Alaro <viquialaro241@gmail.com>
2020-07-05 00:05:54 -0400

    implementacion del metodo de existe pelicula

--- src/app/services/data-local.service.ts
+++ src/app/services/data-local.service.ts
@@ -50,8 +50,17 @@ export class DataLocalService {
     const peliculas = await this.storage.get('peliculas');
     this.peliculas = peliculas || [];
     return this.peliculas;
+  }
 
+  async existePelicula( id ){
+    console.log(id);
+    id = Number(id);
+    console.log(id);
 
+    await this.cargarFavoritos();
+    const existe = this.peliculas.find(peli => peli.id === id);
+
+    return (existe) ? true : false;
   }
 
 
